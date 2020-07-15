687ef5c768c895896b09bc8eb79537368694dcf7
Nicolas Malgat <nicolas.malgat@hotmail.com>
2020-07-15 11:37:10 +0200

    update token de BDD chargé depuis le .env

--- connection.py
+++ connection.py
@@ -1,16 +1,23 @@
 import requests
 import pprint
 import json
+import os
+from dotenv import load_dotenv
 
 #token_auth='votre token ici, recu dans votre boite mail'
-token_auth = 'fed8709b-28c3-4300-9a06-90dea3c52e1c'
+load_dotenv(verbose=True)
+token_auth = os.getenv("TOKEN_AUTH")
 
 url_avec_params = 'https://api.sncf.com/v1/coverage/sncf/stop_areas'
 
-r = requests.get(url_avec_params,auth=(token_auth,''))
-if r.status_code != 200:
-    print('non')
+try:
+    r = requests.get(url_avec_params,auth=(token_auth,''))
+    if r.status_code != 200:
+        raise Exception("La requête possède un code différent de 200: %s" % r.status_code)
+    print('Chargement des stop areas réussi !')
+except:
     exit
 
-with open('stop_aeras.json',"w") as file:
+with open('stop_areas.json',"w") as file:
     file.write(json.dumps(r.json(), indent=2))
+print('Ecriture de stop_areas.json terminée')
