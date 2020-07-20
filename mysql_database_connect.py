4a78851030437b55d19ab78d7a23d3d24a3e6904
Nicolas Malgat <nicolas.malgat@hotmail.com>
2020-07-20 14:52:06 +0200

    update connection à mysql ajout d'une fonction pour créer des tables à partir d'un .sql

--- mysql_database_connect.py
+++ mysql_database_connect.py
@@ -8,16 +8,33 @@ load_dotenv(verbose=True)
 server = os.getenv("MYSQL_HOST")
 username = os.getenv("MYSQL_ROOT")
 password = os.getenv("MYSQL_ROOT_PASSWORD")
+database = r"projet_vacances"
 
 config = {
   'user': username,
   'password': password,
   'host': server,
+  'database': database,
   'raise_on_warnings': True
 }
 
-cnx = mysql.connector.connect(**config)
+class Connection:
+    def __init__(self):
+        self.cnx = mysql.connector.connect(**config)
+        self.cursor = self.cnx.cursor()
 
-print("ça fonctionne !")
+        print('Successfully connected to '+database+' database.')
 
-cnx.close()
+    def create_database(self, sql_path):
+        try:
+            with open('sql/requestsTable.sql', 'r', encoding='utf-8') as sql_file:
+                sql_content = sql_file.read()
+                self.cursor.execute(sql_content, multi=True)
+        except mysql.connector.Error as err:
+            print("Failed creating database: {}".format(err))
+            exit(1)
+        print('Created database.')
+
+if __name__ == "__main__":
+    connect = Connection()
+    connect.create_database('sql/requestsTable.sql')
