54e75592dccc5866c20e787dcf55c92071551dfe
Nicolas Malgat <nicolas.malgat@hotmail.com>
2020-07-21 16:09:34 +0200

    update map visual

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
-from bottle import route, run, template
+from bottle import route, run, static_file
+
 
 @route('/')
 @route('/index')
 def index():
-    return "Hello world !"
+    return static_file('map.html', root='views')
+
+
+@route('*.js')
+def send_javascript(filename):
+    return static_file(filename, root='javascript')
+
 
 run(host='localhost', port=8080)
